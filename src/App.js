5b961efb4a8906e55fa07744e67136004dd87bea
Itz-Official-Khaos <itzofficialkhaos@gmail.com>
2025-04-28 15:07:20 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { userAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore'; 
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA9ScrmWyTEI08hyejIrTzij4w2jk1_keg",
+  authDomain: "kryptonite-services.firebaseapp.com",
+  projectId: "kryptonite-services",
+  storageBucket: "kryptonite-services.firebasestorage.app",
+  messagingSenderId: "915071488615",
+  appId: "1:915071488615:web:6952ad996bbe31cf34bac2",
+  measurementId: "G-8B98FVRHQ3"
+})
+
+const auth = firebase.auth();
+const firebase = firebase.firestore();
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
